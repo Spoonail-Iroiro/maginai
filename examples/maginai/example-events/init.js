0f541f8260032236a7d3085def9ba3486ae01883
Spoonail-Iroiro <38171201+Spoonail-Iroiro@users.noreply.github.com>
2024-06-09 19:10:22 +0900

    Add logging limit to beforeRefresh/afterRefresh in `example-events`

--- examples/maginai/example-events/init.js
+++ examples/maginai/example-events/init.js
@@ -3,12 +3,29 @@
 (function () {
   const logger = maginai.logging.getLogger('events');
 
+  let beforeRefreshCount = 0;
+  let afterRefreshCount = 0;
+
   maginai.events.afterRefresh.addHandler(() => {
-    logger.info('afterRefresh');
+    if (afterRefreshCount < 2) {
+      logger.info('afterRefresh');
+      afterRefreshCount += 1;
+    } else if (afterRefreshCount == 2) {
+      logger.info('afterRefresh (continues to be triggered with every update)');
+      afterRefreshCount += 1;
+    }
   });
 
   maginai.events.beforeRefresh.addHandler(() => {
-    logger.info('beforeRefresh');
+    if (beforeRefreshCount < 2) {
+      logger.info('beforeRefresh');
+      beforeRefreshCount += 1;
+    } else if (beforeRefreshCount == 2) {
+      logger.info(
+        'beforeRefresh (continues to be triggered with every update)'
+      );
+      beforeRefreshCount += 1;
+    }
   });
 
   maginai.events.commandKeyClicked.addHandler((e) => {
